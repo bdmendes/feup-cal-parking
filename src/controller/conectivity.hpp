852288ad5e975a4fd51987e1cced7c24f12924e8
kat-maq <laimirandasantos@gmail.com>
2021-05-24 01:00:17 +0100

    Add conectivity calculator

---
+++ src/controller/conectivity.hpp
@@ -0,0 +1,20 @@
+#ifndef FEUP_CAL_PARKING_CONECTIVITY_HPP
+#define FEUP_CAL_PARKING_CONECTIVITY_HPP
+
+#include "../algorithms/search.hpp"
+#include "../model/StreetMap/StreetMap.h"
+
+bool isStronglyConnected(const StreetMap &map) {
+    return kosaraju(map);
+}
+
+bool isConnected(const std::vector<Node<MapPoint>*> &stopPoints, const StreetMap &map){
+    std::vector<Node<MapPoint> *> res = dfsAll(stopPoints, map);
+    return res.size() == stopPoints.size();
+}
+
+void calculateConectivity(StreetMap &map, const std::vector<Node<MapPoint>*> &stopPoints){
+    return isStronglyConnected(map) || isConnected(stopPoints, map);
+}
+
+#endif //FEUP_CAL_PARKING_CONECTIVITY_HPP
