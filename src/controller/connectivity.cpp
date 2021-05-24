93ddb8509b8742b00affd92c574f8cc29c21cc08
kat-maq <laimirandasantos@gmail.com>
2021-05-24 11:53:55 +0100

    Add function to check if the route can be calculated

--- src/controller/connectivity.cpp
+++ src/controller/connectivity.cpp
@@ -12,6 +12,15 @@ static bool isConnected(const std::vector<Node<MapPoint>*> &stopPoints, const St
     return true;
 }
 
-bool calculateConectivity(StreetMap &map, const std::vector<Node<MapPoint>*> &stopPoints){
-    return isStronglyConnected(map) || isConnected(stopPoints, map);
+static bool existsPath(Node<MapPoint> *source, const std::vector<Node<MapPoint>*> &stopPoints) {
+    std::vector<Node<MapPoint> *> res;
+    dfsVisit(source, stopPoints, res);
+    return res.size() == stopPoints.size();
+}
+
+void calculateConectivity(StreetMap &map, const std::vector<Node<MapPoint>*> &stopPoints, Node<MapPoint> *source){
+    if (isStronglyConnected(map)) std::cout << "The graph is strongly connected.\n";
+    else if (isConnected(stopPoints, map)) std::cout << "The graph is connected.\n";
+    else if (existsPath(source, stopPoints)) std::cout << "The graph isn't connected but there is a possible solution for the problem.\n";
+    else std::cout << "Cannot calculate this route\n";
 }
